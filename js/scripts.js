edefe3e37438f5e0de3295e430409e581adf6a47
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-03-21 01:34:20 -0400

    add CD names and color ramp

--- js/scripts.js
+++ js/scripts.js
@@ -46,60 +46,16 @@ map.on('style.load',function() {
         ['interpolate',
         ['linear'],
         ['get', dataTimestamp],
-        0.5,'#EED322',
-        0.7,'#E6B71E',
-        0.9,'#DA9C20',
-        1.0,'#CA8323',
-        1.2,'#B86B25',
-        1.4,'#A25626',
-        1.6,'#8B4225',
-        1.8,'#723122'
-        ],
-      'fill-outline-color': '#ccc',
-      'fill-opacity': 0.7
-    }
-  })
-})
-
-// function to update the map once the slider value changes
-function updateElements(){
-
-  if (map.getLayer('blackCarbonMean')) {
-    map.removeLayer('blackCarbonMean');
-  }
-  if (map.getSource('black-carbon')) {
-    map.removeSource('black-carbon');
-  }
-  if (map.getLayer('highlight-line')) {
-    map.removeLayer('highlight-line');
-  }
-  if (map.getSource('highlight-feature')) {
-    map.removeSource('highlight-feature');
-  }
-
-  map.addSource('black-carbon', {
-      type: 'geojson',
-      data: 'data/black-carbon.geojson'
-  });
-
-  map.addLayer({
-    'id':'blackCarbonMean',
-    'type': 'fill',
-    'source': 'black-carbon',
-    'layout': {'visibility': 'visible'},
-    'paint': {
-      'fill-color':
-        ['interpolate',
-        ['linear'],
-        ['get', dataTimestamp],
-        0.5,'#EED322',
-        0.7,'#E6B71E',
-        0.9,'#DA9C20',
-        1.0,'#CA8323',
-        1.2,'#B86B25',
-        1.4,'#A25626',
-        1.6,'#8B4225',
-        1.8,'#723122'
+        0.2,'#ffefdc',
+        0.4,'#f5c5ab',
+        0.6,'#f1b093',
+        0.8,'#e78662',
+        1.0,'#e27149',
+        1.2,'#d15e3d',
+        1.4,'#b13925',
+        1.6,'#a02619',
+        1.8,'#7f0101',
+        2.0,'#660202'
         ],
       'fill-outline-color': '#ccc',
       'fill-opacity': 0.7
@@ -134,8 +90,108 @@ function updateElements(){
     if (features.length > 0) {
 
       var hoveredFeature = features[0];
-      var name = hoveredFeature.properties.BoroCD;
-      var mean = hoveredFeature.properties.Winter2012;
+      var name = hoveredFeature.properties.CDname;
+      var mean = hoveredFeature.properties.Summer2012;
+
+      var popupContent = `
+        <div>
+          <b>${name}</b><br/>
+          Mean Absorbance Unit: ${mean}<br/>
+        </div>
+      `
+
+      popup.setLngLat(e.lngLat).setHTML(popupContent).addTo(map);
+      map.getSource('highlight-feature').setData(hoveredFeature.geometry);
+      map.getCanvas().style.cursor = 'pointer';
+    } else {
+      popup.remove();
+      map.getCanvas().style.cursor = '';
+      map.getSource('highlight-feature').setData({
+            "type": "FeatureCollection",
+            "features": []
+        });
+    }
+
+  })
+})
+
+// function to update the map once the slider value changes
+function updateElements(){
+
+  if (map.getLayer('blackCarbonMean')) {
+    map.removeLayer('blackCarbonMean');
+  }
+  if (map.getSource('black-carbon')) {
+    map.removeSource('black-carbon');
+  }
+  if (map.getLayer('highlight-line')) {
+    map.removeLayer('highlight-line');
+  }
+  if (map.getSource('highlight-feature')) {
+    map.removeSource('highlight-feature');
+  }
+
+  map.addSource('black-carbon', {
+      type: 'geojson',
+      data: 'data/black-carbon.geojson'
+  });
+
+  map.addLayer({
+    'id':'blackCarbonMean',
+    'type': 'fill',
+    'source': 'black-carbon',
+    'layout': {'visibility': 'visible'},
+    'paint': {
+      'fill-color':
+        ['interpolate',
+        ['linear'],
+        ['get', dataTimestamp],
+        0.2,'#ffefdc',
+        0.4,'#f5c5ab',
+        0.6,'#f1b093',
+        0.8,'#e78662',
+        1.0,'#e27149',
+        1.2,'#d15e3d',
+        1.4,'#b13925',
+        1.6,'#a02619',
+        1.8,'#7f0101',
+        2.0,'#660202'
+      ],
+      'fill-outline-color': '#ccc',
+      'fill-opacity': 0.7
+    }
+  })
+
+  map.addSource('highlight-feature', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  })
+
+  map.addLayer({
+    id: 'highlight-line',
+    type: 'fill',
+    source: 'highlight-feature',
+    paint: {
+      'fill-color': '#ccc',
+      'fill-opacity': 0.7,
+      'fill-outline-color': 'white'
+    }
+  });
+
+  map.on('mousemove', function (e) {
+
+    var features = map.queryRenderedFeatures(e.point, {
+        layers: ['blackCarbonMean'],
+    });
+
+    if (features.length > 0) {
+
+      var hoveredFeature = features[0];
+      var name = hoveredFeature.properties.CDname;
+      var mean = hoveredFeature.properties[dataTimestamp];
 
       var popupContent = `
         <div>
